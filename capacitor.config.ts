3085f1f65c010237fd0d9491c3479b100e70b588
Zeeshan Asghar <m.zeeshan.asghar@live.com>
2024-03-17 09:23:23 +0500

    initial commit using command: ionic start photo-gallery tabs --type=angular --capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VacDoc',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
